2f32a72d961d4eda195d4537875ae415c5522e42
AstanaRome <alikhan3991@gmail.com>
2022-06-05 16:44:16 +0600

    add crud functions

--- entries/views.py
+++ entries/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import (
     ListView,
     DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
 )
 
 from .models import Entry
@@ -15,4 +19,32 @@ class EntryListView(ListView):
 
 class EntryDetailView(DetailView):
     model = Entry
+
+
+class EntryCreateView(SuccessMessageMixin, CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+    success_message = "Ваша новая запись создана!!"
+
+class EntryUpdateView(SuccessMessageMixin, UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_message = "Ваша запись обновлена!"
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "entry-detail",
+            kwargs={"pk": self.object.pk}
+        )
+
+
+class EntryDeleteView(SuccessMessageMixin, DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
+    success_message = "Ваша запись удалена!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
 # Create your views here.
