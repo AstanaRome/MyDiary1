eb8418e8866c04e4ecae5668743bfbf777a24c13
AstanaRome <alikhan3991@gmail.com>
2022-06-05 11:07:23 +0600

    add css

--- entries/views.py
+++ entries/views.py
@@ -7,9 +7,11 @@ from django.views.generic import (
 
 from .models import Entry
 
+
 class EntryListView(ListView):
     model = Entry
     queryset = Entry.objects.all().order_by("-date_created")
+
 
 class EntryDetailView(DetailView):
     model = Entry
